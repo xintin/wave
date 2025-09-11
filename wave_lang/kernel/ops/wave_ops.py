026983537582f8414d189753265784940fc00a6a
William G Hatch <william@hatch.uno>
2025-09-11 11:31:48 -0600

    Capture locations for Iterate and Conditional (#290)
    This just adds the location capturing logic from the default CustomOp
    handler to these two ops that define their own handler.
    
    Signed-off-by: William G Hatch <william@hatch.uno>

--- wave_lang/kernel/ops/wave_ops.py
+++ wave_lang/kernel/ops/wave_ops.py
@@ -1854,6 +1854,7 @@ class Conditional(NestedRegionOp):
             node._add_proxy_to_graph(graph)
             node.fx_node.node.tkw_op = cls
             node.fx_node.node.tkw_op_name = cls.tkw_op_name
+            node.fx_node.node.location = capture_location(graph.location_capture_config)
             graph.subgraphs[subgraph_name].parent_op = node.fx_node.node
             return node.fx_node
 
@@ -1908,6 +1909,7 @@ class Iterate(NestedRegionOp):
             node._add_proxy_to_graph(graph)
             node.fx_node.node.tkw_op = cls
             node.fx_node.node.tkw_op_name = cls.tkw_op_name
+            node.fx_node.node.location = capture_location(graph.location_capture_config)
             graph.subgraphs[subgraph_name].parent_op = node.fx_node.node
             return node.fx_node
 
